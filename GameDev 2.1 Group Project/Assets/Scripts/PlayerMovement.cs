9ff8e16cdfbc4ec62af64a3ccda4f6ee5e3ba5a4
Dylan Hopkins <A00284332@student.ait.ie>
2021-10-25 02:08:36 +0100

    Fundamentals contd.
    Got player horizontal movement working with Unity build in Input Manager.
    
    Fixed issue with background bleeding through terrain tiles.

--- GameDev 2.1 Group Project/Assets/Scripts/PlayerMovement.cs
+++ GameDev 2.1 Group Project/Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Rigidbody2D player;
+    public float spdHorizontal = 7.0f;
 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         CharacterMovement();
     }
 
-    //Character Movement Script
-    void CharacterMovement() {
+    /* Character Movement Script 
+     * 
+     * Rather than hard coding the input keys, use the inputs defined inside unity.
+     * Go to Edit > Project Settings > Input Manager > Axes to see the different types available.
+     * This can be better as it allows for multiple types of control (Keyboard, Joystick etc.) without hard coding for each.
+     * 
+     */
+    private void CharacterMovement() 
+    {
+        //Set float dirX equal to the horizontal axis ranging between -1 and 1, allowing analog support.
+        float dirHorizontal = Input.GetAxisRaw("Horizontal");
 
-        //Player Jump
-        if (Input.GetKeyDown("space"))
+        //Player Move Left(-1 to -0.1) / Right (0.1 to 1) by using horizontal as a multiplier, times the speed.
+        player.velocity = new Vector2(dirHorizontal * spdHorizontal, player.velocity.y);
+
+        //Player Jump from Input manager, rather than hard coding spacebar.
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0);
+            player.velocity = new Vector2(player.velocity.x, 14f);
         }
+
     }
 }
