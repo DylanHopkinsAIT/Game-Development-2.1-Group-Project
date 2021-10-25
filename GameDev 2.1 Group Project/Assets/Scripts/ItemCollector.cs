3a3fd291ddf56e7d6b8776b49ea0885d9945b16f
dsutov <93036816+dsutov@users.noreply.github.com>
2021-10-25 18:27:40 +0100

    Added cherries and an on screen counter
    Cherries can now be collected by the player
    A UI element displays the amount of cherries the player collected

---
+++ GameDev 2.1 Group Project/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // imports code to allow Text to be used 
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    [SerializeField] private Text cherriesText;
+
+    /* This method is called when the player collides with the cherry 
+     */
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry")) // checks if the tag is "Cherry"
+        {
+            Destroy(collision.gameObject); // destroys the cherry object
+            cherries++; // increases the cherry counter
+            cherriesText.text = "Cherries: " + cherries; // updates the UI text on screen
+        }
+    }
+}
