930d2063063a7c5c99fcb328cb48be0e072df13f
Dylan Hopkins <A00284332@student.ait.ie>
2021-10-25 03:06:46 +0100

    Animation & Camera
    Player now has idle and running animations working. Running Left & right plays the appropriate animation.
    
    Camera has script to follow player.

--- GameDev 2.1 Group Project/Assets/Scripts/PlayerMovement.cs
+++ GameDev 2.1 Group Project/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D player;
-    public float spdHorizontal = 7.0f;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    //Using [SerializeField] allows the values to be changed in unity, but is better practice than just making the variable public.
+    [SerializeField] private float moveSpeed = 7.0f;
+    [SerializeField] private float jumpSpeed = 14.0f;
+    private float dirHorizontal = 0f;
 
     // Start is called before the first frame update
     private void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         CharacterMovement();
+        AnimationState();
     }
 
     /* Character Movement Script 
@@ -29,16 +38,41 @@ public class PlayerMovement : MonoBehaviour
     private void CharacterMovement() 
     {
         //Set float dirX equal to the horizontal axis ranging between -1 and 1, allowing analog support.
-        float dirHorizontal = Input.GetAxisRaw("Horizontal");
+        dirHorizontal = Input.GetAxisRaw("Horizontal");
 
         //Player Move Left(-1 to -0.1) / Right (0.1 to 1) by using horizontal as a multiplier, times the speed.
-        player.velocity = new Vector2(dirHorizontal * spdHorizontal, player.velocity.y);
+        player.velocity = new Vector2(dirHorizontal * moveSpeed, player.velocity.y);
 
         //Player Jump from Input manager, rather than hard coding spacebar.
         if (Input.GetButtonDown("Jump"))
         {
-            player.velocity = new Vector2(player.velocity.x, 14f);
+            player.velocity = new Vector2(player.velocity.x, jumpSpeed);
         }
 
+    }
+
+    //Test if player is moving horizontally to determine what animation should play
+    private void AnimationState() 
+    {
+        //Moving Right (dirHorizontal is between 0.1 and 1)
+        if (dirHorizontal > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+
+        }
+
+        //Moving Left (dirHorizontal is between -1 and -0.1)
+        else if (dirHorizontal < 0)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+
+        //Idle (dirHorizontal is 0)
+        else
+        {
+            anim.SetBool("running", false);
+        }
     }
 }
